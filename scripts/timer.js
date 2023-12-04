483ff8fd4cdd1f7ae94c59c6b7b3a5adf62ec780
Agustinlv <agustinlv@hotmail.com>
2023-12-04 12:45:48 -0300

    Algunas mejoras

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,8 +1,10 @@
 const timerContainerDOM = document.getElementById('timerContainer');
 const timerDOM = document.createElement('div');
+const touchControlDOM = document.getElementById('touchControl');
 const startButtonDOM = document.getElementById('startButton');
 const resetButtonDOM = document.getElementById('resetButton');
 const hideButtonDOM = document.getElementById('hideButton');
+const hideButtonImgDOM = document.getElementById('hideButtonImg');
 const headerDOM = document.getElementById('header');
 const hoursDOM = document.getElementById('hours');
 const minutesDOM = document.getElementById('minutes');
@@ -16,29 +18,32 @@ timerDOM.id = "timer";
 
 startButtonDOM.addEventListener('click', startTimer);
 resetButtonDOM.addEventListener('click', resetTimer);
-hideButtonDOM.addEventListener('click', hideNavbar);
-
+hideButtonDOM.addEventListener('click', hideControls);
+touchControlDOM.addEventListener('click', startTimer);
 
 function pad(number, padding){
     return String(number).padStart(padding, '0');
 };
 
-function hideNavbar(){
-    navbarHidden = !navbarHidden;
+function hideControls(){
 
-    if (navbarHidden)
-    {
-        headerDOM.style.display = "none";
-        hideButtonDOM.textContent = "show controls";
-        timerDOM.style.height = "98vh";
-    }
-    else
-    {
-        headerDOM.style.display = "flex";
-        hideButtonDOM.textContent = "hide controls";
-        timerDOM.style.height = "90vh";
-    };
+    headerDOM.style.display = "none";
+    hideButtonDOM.removeEventListener('click', hideControls);
+    hideButtonDOM.addEventListener('click', showControls);
+    hideButtonImgDOM.src = "./img/show.png";
+    timerDOM.style.height = "98vh";
+
 };
+
+function showControls(){
+        
+    headerDOM.style.display = "flex";
+    hideButtonDOM.removeEventListener('click', showControls);
+    hideButtonDOM.addEventListener('click', hideControls);
+    hideButtonImgDOM.src = "./img/hide.png";
+    timerDOM.style.height = "90vh";
+
+}
 
 function saveTimer(){
     localStorage.setItem("savedTimer", JSON.stringify(timer));
@@ -64,6 +69,18 @@ function startTimer(){
     pausedTimer = !pausedTimer;
 
     setResetFields();
+
+    if (!pausedTimer){
+        
+        hideControls();
+
+        startButtonDOM.textContent = "pause";
+
+    } else {
+
+        startButtonDOM.textContent = "start";
+
+    }
 };
 
 function resetTimer(){
@@ -113,4 +130,5 @@ setInterval(()=>{
     };
     
     timerMain();
+
 },1000);
